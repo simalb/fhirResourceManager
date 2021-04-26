d5052d2a8507bb96d65c5a8d7ccd6c0577f4574d
esimalb <simona.albanesi@ericsson.com>
2021-04-26 22:47:42 +0200

    review

--- fhirResourceManager-servlet/src/main/java/frm/bean/TransferFhirPatientHandler.java
+++ fhirResourceManager-servlet/src/main/java/frm/bean/TransferFhirPatientHandler.java
@@ -2,14 +2,14 @@ package frm.bean;
 
 public interface TransferFhirPatientHandler {
 
-    public static final String PUBLIC_TEST_SERVER_URI = "http://hapi.fhir.org/baseR4/Patient/";
+    String PUBLIC_TEST_SERVER_URI = "http://hapi.fhir.org/baseR4/Patient/";
 
     //JAVA TEST - 1 READ AND TRANSFER A FHIR RESOURCE
-    public boolean transferFhirPatient(String fhirUrl);
+    boolean transferFhirPatient(String fhirUrl);
 
     //2 GET THE COPY
-    public String transferedPatient(String fhirUrl);
+    String transferedPatient(String fhirUrl);
 
     //4 FHIR TEST SERVER
-    public String createPatientOnPublicFhirServer(String fhirPatientJson);
+    String createPatientOnPublicFhirServer(String fhirPatientJson);
 }
